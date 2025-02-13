7b8e5f40c5903477d2d8634bcb1a9482bbec9179
felip-SN <felipenascimentooudri@gmail.com>
2025-02-12 22:38:45 -0300

    Inicio do projeto; baixado as ferramentas necessarias; modelos criados; contexto iniciado

---
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalogo.Models
+{
+    public class Produto
+    {
+        public int ProdutoId { get; set; }
+        public string? ProdutoNome { get; set; }
+        public string? ProdutoDescricao { get; set; }
+        public decimal Preco { get; set; }
+        public string? ImagemUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
